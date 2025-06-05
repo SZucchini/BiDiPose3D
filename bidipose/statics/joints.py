bda1b50e9f4d871285d70101288119aebc10e3f9
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2025-06-05 15:04:55 +0900

    add joints and bone def

---
+++ bidipose/statics/joints.py
@@ -0,0 +1,21 @@
+"""Static joint definitions."""
+
+h36m_joints_name_to_index = {
+    "PELVIS": 0,
+    "R_HIP": 1,
+    "R_KNEE": 2,
+    "R_ANKLE": 3,
+    "L_HIP": 4,
+    "L_KNEE": 5,
+    "L_ANKLE": 6,
+    "SPINE": 7,
+    "THORAX": 8,
+    "NECK": 9,
+    "HEAD": 10,
+    "L_SHOULDER": 11,
+    "L_ELBOW": 12,
+    "L_WRIST": 13,
+    "R_SHOULDER": 14,
+    "R_ELBOW": 15,
+    "R_WRIST": 16,
+}
