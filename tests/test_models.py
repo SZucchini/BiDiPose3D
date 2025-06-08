0ff5f6b0997204033c41ee038009087a1062451a
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2025-06-08 17:26:08 +0900

    fixed test timestep shape and model forward pass

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,7 +10,13 @@ from bidipose.models.MotionAGFormer.model import MotionAGFormer
 from bidipose.models.MotionBERT.model import DSTformer
 
 
-@pytest.mark.parametrize("t", [None, torch.randn(4, 1)])
+@pytest.mark.parametrize(
+    "t",
+    [
+        None,
+        torch.randn(4),
+    ],
+)
 def test_motion_agformer(t):
     """Test the MotionAGFormer model."""
     model = MotionAGFormer(n_layers=2, dim_feat=32, dim_in=6, n_frames=88)
@@ -31,7 +37,7 @@ def test_motion_agformer(t):
     assert torch.allclose(trans_norm, torch.ones_like(trans_norm), atol=1e-6), f"Translation norm: {trans_norm}"
 
 
-@pytest.mark.parametrize("t", [None, torch.randn(4, 1)])
+@pytest.mark.parametrize("t", [None, torch.randn(4)])
 def test_dstformer(t):
     """Test the DSTformer model."""
     model = DSTformer(dim_feat=512, mlp_ratio=2, norm_layer=partial(nn.LayerNorm, eps=1e-6))
